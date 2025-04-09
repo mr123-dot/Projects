c8bf803d1945ef52f62af1e2f9b03e9234b6dc05
mr123-dot <mohamedrekhadda@gmail.com>
2025-04-10 00:14:42 +0100

    projets de stage

--- stage/lib/hello.dart
+++ stage/lib/hello.dart
@@ -5,7 +5,5 @@ void main() {
   int randomNumber = random.nextInt(100); // Generates a random number between 0 and 99
   print('Random number: $randomNumber');
 
-  List<String> greetings = ['Hello', 'Hi', 'Hey', 'Welcome', 'Greetings'];
-  String randomGreeting = greetings[random.nextInt(greetings.length)];
-  print('$randomGreeting, Dart!');
+  
 }
