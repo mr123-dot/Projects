e8864584c40ed2866fdf54998e741c7f637922da
mr123-dot <mohamedrekhadda@gmail.com>
2025-04-15 22:48:09 +0100

    cv

--- stage/android/app/build.gradle.kts
+++ stage/android/app/build.gradle.kts
@@ -1,44 +1,23 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+    kotlin("android")
 }
 
 android {
-    namespace = "com.example.stage"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
+    namespace = "com.example.yourapp" // change si nécessaire
+    compileSdk = 33
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.stage"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        applicationId = "com.example.yourapp" // change si nécessaire
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
+    ndkVersion = "25.2.9519653"
 }
 
-flutter {
-    source = "../.."
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.0") // ou autre selon ton projet
 }
--- stage/lib/hello.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:math';
-
-void main() {
-  final random = Random();
-  int randomNumber = random.nextInt(100); // Generates a random number between 0 and 99
-  print('Random number: $randomNumber');
-
-  
-}
